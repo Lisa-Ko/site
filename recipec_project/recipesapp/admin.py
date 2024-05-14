55f68835c48cc4288dfca2bd32463cc851fb2537
drkop <satanatyn@yandex.ru>
2024-05-14 03:03:27 +0300

    problem

---
+++ recipec_project/recipesapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Recipe,RecipeCategory,RecipeCategoryRelationship
+
+admin.site.register(Recipe)
+
+admin.site.register(RecipeCategory)
+
+admin.site.register(RecipeCategoryRelationship)
+# Register your models here.
